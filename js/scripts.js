0eb66d4e41998de0f0c6dffc21250ab97d14ad03
erick <erickkirui653@gmail.com>
2018-07-13 17:41:22 +0300

    jquary functions added

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,8 @@
-$(document).ready(function() {
-    $("form#ping-pong").submit(function(event) {
-      event.preventDefault();
-      for (var x=1; x <= 100; x++){
-    if( x % 3 && x % 5 ) {
-        document.write(x);
-    } else {
-        if( x % 3 == 0 ) {
-            document.write("ping");
-        }
-        if( x % 5 == 0 ) {
-            document.write("pong");
-        }
-    }
-    document.write('<br>'); //line breaks to enhance output readability
-}​
+var trippler=function(Number){
+  for(Number=1;Number<30;Number ++){
+    if(Number%3===0 Number%5===0){ alert("pingpong");
+  }else if(Number%3===0){ alert("ping");
+}else if (Number%5===0){
+                      aleart("pong")
+                  }else{ alert(Number)}
+}
