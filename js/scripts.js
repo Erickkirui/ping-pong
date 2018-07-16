4cdd69948aa3e8a7a0770c1e26a68cc0942b7950
erick <erickkirui653@gmail.com>
2018-07-16 10:41:49 +0300

    moving enjoy click

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,29 @@
 var trippler=function(Number){
   for(Number=1;Number<30;Number ++){
-    if(Number%3===0 Number%5===0){ alert("pingpong");
+    if(Number%3===0 && Number%5===0){ alert("pingpong");
   }else if(Number%3===0){ alert("ping");
 }else if (Number%5===0){
                       aleart("pong")
-                  }else{ alert(Number)}
-}
+                  }else{ alert(Number)};};};
+$(document).ready(function(){
+  $("form#number").submit(function(event){
+    event.preventDefault();
+    var Number=parseInt($("inout#put").val());
+    var Result=deter(Number)
+    $("ul#trippler").append("<li>result</li>");
+  });
+});
+
+
+
+
+
+$("ul").hover(function(){
+    $(this).css("background-color", "yellow");
+    }, function(){
+    $(this).css("background-color", "pink");
+});$("ul").hover(function(){
+    $(this).css("background-color", "yellow");
+    }, function(){
+    $(this).css("background-color", "pink");
+});
